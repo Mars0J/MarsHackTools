32fc56cb0a0221f01b2706708fde91c3a93dd931
Mars0J <70023143+Mars0J@users.noreply.github.com>
2021-01-17 17:57:11 +0300

    Add files via upload

---
+++ marshacktools.py
@@ -0,0 +1,154 @@
+import os
+import time
+import sys
+
+print ("\033[34m Aracı Başlatmak İçin 'Enter' Tuşuna Basınız ")
+def banner():
+    os.system("clear")
+    print ("""\033[1;31m
+ ███▄ ▄███▓ ▄▄▄       ██▀███    ██████  
+▓██▒▀█▀ ██▒▒████▄    ▓██ ▒ ██▒▒██    ▒ 
+▓██    ▓██░▒██  ▀█▄  ▓██ ░▄█ ▒░ ▓██▄   
+▒██    ▒██ ░██▄▄▄▄██ ▒██▀▀█▄    ▒   ██▒
+▒██▒   ░██▒ ▓█   ▓██▒░██▓ ▒██▒▒██████▒▒
+░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▓▒ ▒ ░
+░  ░      ░  ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒  ░ ░
+░      ░     ░   ▒     ░░   ░ ░  ░  ░  
+       ░         ░  ░   ░           ░  """ + """\033[1;33m
+
+██╗  ██╗ █████╗  ██████╗██╗  ██╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+███████║███████║██║     █████╔╝        ██║   ██║   ██║██║   ██║██║     ███████╗
+██╔══██║██╔══██║██║     ██╔═██╗        ██║   ██║   ██║██║   ██║██║     ╚════██║
+██║  ██║██║  ██║╚██████╗██║  ██╗       ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+"""
++ """\033[1;34m \n  Made By Mars """)
+    print ("\n\033[1;32m====================================")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[1]\033[35m MarsLayer 7 ")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[2]\033[35m Mars Hack Camera")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[3]\033[35m MarsWebAdminPanel Finder")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[4]\033[35m Gmail Brute ")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[5]\033[35m LoadNumber ")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[6]\033[35m Email Bomb ")
+    time.sleep(2)
+    print()
+    print ("\033[1;33m[7]\033[35m Brute İnsta ")
+    time.sleep(1)
+    print()
+    print ("\033[31m[E] Exit ")
+    time.sleep(2)
+    print ("\033[1;32m\n====================================")
+
+    a = input("\n\033[36m Marshacktools \033[33m > ")
+    if a=='1':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/MarsLayer7")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m MarsLayer7 Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='2':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/MarsHackCamera")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m Mars Hack Camera Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='3':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/MarsWebAdminPanel-Finder")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m MarsLayer7 Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='4':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/Gmail-Brute")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m Gmail-Brute Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+
+    if a=='5':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/LoadNumber")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m LoadNumber Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='6':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/Email-Bomb")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m Email Bomb Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='7':
+       os.system("clear")
+       os.system("git clone https://github.com/Mars0J/Brute-İnsta-")
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;32m[+]\033[36m Brute İnsta Aracı Başarıyla İndi. ")
+       time.sleep(1)
+       banner()
+    if a=='e':
+       def slowprint(s):
+           for c in s + '\n' :
+               sys.stdout.write(c)
+               sys.stdout.flush()
+               time.sleep(10. / 100)
+       slowprint("\033[1;31m ▇◤▔▔▔▔▔▔▔◥▇")
+       slowprint("\033[1;32m ▇▏◥▇◣┊◢▇◤▕▇")
+       slowprint("\033[1;33m ▇▏▃▆▅▎▅▆▃▕▇")
+       slowprint("\033[1;34m ▇▏╱▔▕▎▔▔╲▕▇")
+       slowprint("\033[1;35m ▇▏╱▔▕▎▔▔╲▕▇")
+       slowprint("\033[1;31m ▇◣◣▃▅▎▅▃◢◢▇")
+       slowprint("\033[1;32m ▇▇◣◥▅▅▅◤◢▇▇")
+       slowprint("\033[1;33m ▇▇▇◣╲▇╱◢▇▇▇")
+       time.sleep(5)
+       print("Görüşmek Üzere")
+
+input()
+banner()
+
+
